1fee98184c446a1509382132befa047f7bcd5f49
rizqiirfand <rizqiirfan23@gmail.com>
2023-12-12 06:54:47 +0700

    tes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/firebase-messaging-sw.js")
+    .then((registration) => {
+      console.log("Firebase Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Failed to register Firebase Service Worker:", error);
+    });
+}
