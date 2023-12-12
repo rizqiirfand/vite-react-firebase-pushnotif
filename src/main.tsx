a1c118e7380140d0e5bbf047f6cc2901a92879df
rizqiirfand <rizqiirfan23@gmail.com>
2023-12-12 07:01:53 +0700

    tes

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("../firebase-messaging-sw.js")
+    .register("./firebase-messaging-sw.js")
     .then((registration) => {
       console.log("Firebase Service Worker registered with scope:", registration.scope);
     })
