abaa4df5d8a48d8c8e234f3f6691a8890ed03bb7
rizqiirfand <rizqiirfan23@gmail.com>
2024-01-02 10:13:03 +0700

    back

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,18 +1,22 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 
-// Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
-  apiKey: "AIzaSyDRPEs_iDZ-6AOpxUIqzd0ZdQojec4Aliw",
-  authDomain: "streamline-pn.firebaseapp.com",
-  projectId: "streamline-pn",
-  storageBucket: "streamline-pn.appspot.com",
-  messagingSenderId: "509129972777",
-  appId: "1:509129972777:web:c643c71ac40ec8a94b5038",
-  measurementId: "G-M96SZHZG3L",
-};
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: "AIzaSyB77cvnFN6G80ywcJ08yv_ZWXBTUenynH4",
+  authDomain: "serious-mariner-356010.firebaseapp.com",
+  projectId: "serious-mariner-356010",
+  storageBucket: "serious-mariner-356010.appspot.com",
+  messagingSenderId: "189773937651",
+  appId: "1:189773937651:web:5e3f07292214621cd7f086",
+  measurementId: "G-GVVLZ1PVX5",
+});
 
-firebase.initializeApp(firebaseConfig);
-
-// Retrieve firebase messaging
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
